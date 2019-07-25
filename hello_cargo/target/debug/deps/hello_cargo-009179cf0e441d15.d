6aa202ea9d7310c29a7f927983e3042087467397
Edgar Adrián <edgar.ruiz@devant.com.mx>
2019-07-25 12:24:39 -0500

    Building and Running a Cargo Project

---
+++ hello_cargo/target/debug/deps/hello_cargo-009179cf0e441d15.d
@@ -0,0 +1,5 @@
+/Users/edgarruiz/Desarrollo/Rust/RustStudy/hello_cargo/target/debug/deps/hello_cargo-009179cf0e441d15: src/main.rs
+
+/Users/edgarruiz/Desarrollo/Rust/RustStudy/hello_cargo/target/debug/deps/hello_cargo-009179cf0e441d15.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/edgarruiz/Desarrollo/Rust/RustStudy/hello_cargo/target/debug/hello_cargo.dSYM: /Users/edgarruiz/Desarrollo/Rust/RustStudy/hello_cargo/src/main.rs
